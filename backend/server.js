0777bfdd06adefc079462d03b958a4a4e1cffcf1
Lucianna Mendonca <luciannamend@gmail.com>
2025-02-02 15:23:52 -0500

    Adjustment on backend config and frontend layout

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,16 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+// express
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+// port
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => res.send("MERN Stack API Running"));
 
+// mongodb
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
